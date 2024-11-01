f9a94d32855b2c9eb0a313c37576d75ff4c595f1
Zaib G <shahzaibshazada123@gmail.com>
2024-11-01 03:23:36 -0700

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi1.Data;
+using webapi1.Models.Entities;
+
+namespace webapi1.Controllers
+{
+    //localhost:xxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(dbContext.Employees.ToList());
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi1.Models.Entities;
+
+namespace webapi1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+    }
+}
